a2f13d2faddfe06a76aea1edbac9c9a629e96751
Mohit Kamdi <mohitkamdi317@gmail.com>
2023-11-05 13:47:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App";
+const App = () => {
+  const [data, setData] = useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  useEffect(() => {
+    fetch("https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.hits);
+      });
+  }, []);
+
+  return<div className="card-group">
+    {data.map((res) => (
+      <div
+        className="card"
+        style={{ width: "300px", height: "300px", border: "1px solid #464646" }}
+      >
+        <>
+          <img src={res?.url} />
+          <h1>Title: {res?.title}</h1>
+          <h1>author: {res?.author}</h1>
+          <h1>
+            created_at: {new Date(res?.created_at).toDateString("DD-MM-YYYY")}
+          </h1>
+        </>
+      </div>
+    ))}
+  </div>;
+};
 
 export default App;
+
+// title
+// author
+// created_at
+// url
